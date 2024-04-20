66960927dd115d8707f665a0518c3630c6decc60
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2024-04-20 00:53:52 -0400

    Fixed the problem with images not showing

--- resources/js/Pages/MoviesView.vue
+++ resources/js/Pages/MoviesView.vue
@@ -15,17 +15,48 @@ onMounted(async () => {
         })
         .catch((error) => console.error(error));
 });
+
+const getCardStyle = (poster) => {
+    return {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(http://localhost:8000/storage/${poster})`,
+        backgroundSize: 'cover !important',
+        backgroundClip: 'border-box !important',
+        color: 'white !important',
+        position: 'relative !important',
+        zIndex: '2 !important'
+    };
+};
 </script>
 
+<style scoped>
+#custom-card {
+    height: 100%;
+    position: relative;
+}
+
+#custom-card::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    border-radius: inherit;
+    z-index: 1;
+}
+</style>
+
 <template>
-    <!--    loop over list of movies and display the poster as the background image, movie title, and movie description-->
     <div class="container">
         <h2 class="mb-5">Movies</h2>
-        <div class="row gx-4 gy-5">
-            <div class="card col-md-4" v-for="movie in movies" :key="movie.id">
-                <div class="card-body" :style="{ backgroundImage: 'url(' + movie.poster + ')' }">
-                    <h3>{{ movie.title }}</h3>
-                    <p>{{ movie.description }}</p>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col d-flex" v-for="movie in movies" :key="movie.id">
+                <div class="card" id="custom-card">
+                    <div class="card-body" :style="getCardStyle(movie.poster)">
+                        <h3>{{ movie.title }}</h3>
+                        <p>{{ movie.description }}</p>
+                    </div>
                 </div>
             </div>
         </div>

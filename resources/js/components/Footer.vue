c21ea045fbfdf9e2253caa5f1c4bf260ddea8da8
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2024-04-24 14:24:26 -0400

    made footer position fixed to the bottom of the screen

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <footer class="bg-light py-3">
+    <footer class="bg-light py-3 fixed-bottom">
         <div class="container">
             Copyright &copy; 2024, COMP3385 Web Dev Superstars ✨
         </div>
